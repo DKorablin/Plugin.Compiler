113f7f58e886b5449f62fdcc9028f1ae1a8954a4
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 00:31:52 +0300

    Unified plugins deployment

--- Plugin.Compiler/Properties/AssemblyInfo.cs
+++ Plugin.Compiler/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("425f8b0c-f049-44ee-8375-4cc874d6bf94")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,15 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=102")]
 #else
 
-[assembly: AssemblyTitle("Plugin.Compiler")]
 [assembly: AssemblyDescription("Compile runtime .NET code for various functions")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.Compiler")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2010-2024")]
 #endif
 
